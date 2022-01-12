f583cfa0656c92973c78f621092165b69399af8c
zahra <zqarebaqi@gmail.com>
2022-01-12 21:18:14 +0330

    hw10

---
+++ 1/1.js
@@ -0,0 +1,11 @@
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const p = document.getElementById("thep");
+
+
+let clasx= document.getElementsByClassName("fill-x")
+let claso = document.getElementsByClassName("fill-o")
+
+smalldiv.addEventListener("click" function () {
+      smalldiv.innerText = clasx;
+
+});
